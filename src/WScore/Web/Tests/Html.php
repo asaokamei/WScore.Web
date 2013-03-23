30fe78d192434ef07ca957bb328cc72cb31d5116
Asao <kamei@workspot.jp>
2013-03-24 07:17:10 +0900

    a tool for Html testing added.

---
+++ src/WScore/Web/Tests/Html.php
@@ -0,0 +1,65 @@
+<?php
+namespace WScore\Web\Tests;
+
+class Html
+{
+    /**
+     * @param string $file
+     * @return string
+     */
+    public static function getFileContents( $file )
+    {
+        return file_get_contents( $file );
+    }
+
+    /**
+     * @param \WScore\Web\FrontMC $app
+     * @param array|string        $server
+     * @param array               $post
+     * @return string
+     */
+    public static function getAppContents( $app, $server, $post=array() )
+    {
+        if( !is_array( $server ) ) {
+            $server = array(
+                'REQUEST_METHOD' => 'GET',
+                'SCRIPT_NAME'    => '/test/app.php',
+                'REQUEST_URI'    => '/test/' . $server,
+            );
+        }
+        $app->pathInfo( $server );
+        /** @var $response \WScore\Web\Http\Response */
+        $response = $app->using( $post )->run();
+        return $response->content;
+    }
+
+    /**
+     * @param string $html
+     * @return array
+     */
+    public static function extractHtmlTestMatches( $html )
+    {
+        $startTag = '<!-- HtmlTest: matchStart -->';
+        $endTag   = '<!-- HtmlTest: matchEnd -->';
+        if( preg_match_all( "/{$startTag}(.*?){$endTag}/ms", $html, $match ) ) {
+            $html = $match[1];
+        }
+        return $html;
+    }
+
+    /**
+     * @param string $html
+     * @return array
+     */
+    public static function extractHtmlTestNeedles( $html )
+    {
+        $needles = array();
+        $startTag = '<!-- HtmlTest: Needle=';
+        $endTag   = ' -->';
+        if( preg_match( "/({$startTag}.*?{$endTag})/ms", $html, $match ) ) {
+            $needles = $match[1];
+        }
+        return $needles;
+    }
+
+}
