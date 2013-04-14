1c93d59a7aef0ed8e652dd0c17dd4b7c812c1f2b
Asao <kamei@workspot.jp>
2013-04-14 12:56:16 +0900

    removed old Loader related test; no tests left...

--- tests/WScore/tests/All_Web_Tests.php
+++ tests/WScore/tests/All_Web_Tests.php
@@ -8,9 +8,6 @@ class All_Web_Tests
     public static function suite()
     {
         $suite = new \PHPUnit_Framework_TestSuite( 'all tests for WScore\'s Web' );
-        $suite->addTestFile( __DIR__ . '/FrontMC_Test.php' );
-        $suite->addTestFile( __DIR__ . '/Loader/Matcher_Test.php' );
-        $suite->addTestFile( __DIR__ . '/Loader/MatchSelfConfig_Test.php' );
 
         return $suite;
     }
