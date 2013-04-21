e28a7aa45ed58da8164286f05614aded0f435fba
Asao <kamei@workspot.jp>
2013-04-21 20:25:49 +0900

    code for generating navi-menu for bootstrap. copied from AmidaMVC.

---
+++ src/WScore/Web/View/Menu.php
@@ -0,0 +1,78 @@
+<?php
+namespace WScore\Web\View;
+
+class Menu
+{
+    public $pathInfo;
+    
+    public $baseUrl;
+    
+    public $score;
+    
+    public $menu = array();
+
+    /**
+     * @param array $menu
+     * @param string $pathInfo
+     * @return $this
+     */
+    public function setMenu( $menu, $pathInfo )
+    {
+        $this->pathInfo = $pathInfo;
+        $this->score = $this->prepMenu( $menu );
+        $this->menu = $menu;
+        return $this;
+    }
+
+    /**
+     * @return array
+     */
+    public function getMenu() {
+        return $this->menu;
+    }
+
+    /**
+     * @return int
+     */
+    public function getScore() {
+        return $this->score;
+    }
+        
+    /**
+     * @param array $menu
+     * @return int
+     */
+    private function prepMenu( &$menu )
+    {
+        $max_score = -1;
+        foreach( $menu as &$item ) 
+        {
+            if( isset( $item[ 'url' ] ) ) {
+                $item[ 'score' ] = $this->score( $item['url'] );
+                if( $item[ 'score' ] >  $max_score ) $max_score = $item[ 'score' ];
+            }
+            if( isset( $item[ 'pages' ] ) && is_array( $item[ 'pages' ] ) ) {
+                $score = $this->prepMenu( $item[ 'pages' ] );
+                if( $score > $max_score ) $max_score = $score;
+                if( $score > $item[ 'score' ] ) $item[ 'score' ] = $score;
+            }
+        }
+        return $max_score;
+    }
+    
+    /**
+     * @param string $url
+     * @return int
+     */
+    private function score( $url ) 
+    {
+        $pathInfo = $this->pathInfo;
+        $pathLength = strlen( $pathInfo );
+        for( $i = 0; $i < $pathLength; $i++ ) {
+            if( !isset( $url[$i] ) || $pathInfo[$i] !== $url[$i] ) break;
+        }
+        $diff = min( 100, strlen( $url ) - $pathLength );
+        $score = $i * 100 + 100 - $diff;
+        return $score;
+    }
+}
---
+++ src/WScore/Web/View/NavBarBootstrap.php
@@ -0,0 +1,127 @@
+<?php
+namespace WScore\Web\View;
+
+class NavBarBootstrap
+{
+    protected $menu = array();
+    
+    protected $max_score = null;
+    
+    protected $tabs = array(
+        'topUl'   => '<ul class="nav nav-tabs">',
+        'subUl'   => '<ul class="dropdown-menu">',
+        'divider' => '<li class="divider"></li>',
+        'endUl'   => '</ul>',
+        'liItem'  => '<li class="%3$s"><a href="%2$s">%1$s</a></li>',
+        'liBlank' => '<li class="%3$s"><a href="%2$s" target="_blank">%1$s</a></li>',
+        'liSub'   => '<li class="dropdown%3$s">
+            <a class="dropdown-toggle" data-toggle="dropdown">%1$s<b class="caret"></b></a>%2$s
+            </li>',
+        'icon' => '<i class="icon-%s"></i>',
+    );
+    
+    protected $pill = array();
+    
+    protected $tags = array();
+
+    function __construct() 
+    {
+        $this->pill = $this->tabs;
+        $this->pill[ 'topUl' ] = '<ul class="nav nav-pills">';
+        $this->tags = $this->tabs;
+    }
+    /**
+     * @param array $menu
+     * @param $max_score
+     * @return string
+     */
+    function setMenu( $menu, $max_score=null ) 
+    {
+        $this->menu      = $menu;
+        $this->max_score = $max_score;
+        return $this;
+    }
+
+    /**
+     * @param string $name
+     * @return $this
+     */
+    function setTags( $name ) 
+    {
+        if( isset( $name ) && isset( $this->$name ) && is_array( $this->$name ) ) {
+            $this->tags = & $this->$name;
+        }
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    function draw() 
+    {
+        $html  = $this->ul( $this->menu );
+        return $html;
+    }
+
+    /**
+     * @param string $name
+     * @return mixed|string
+     */
+    private function text( $name ) 
+    {
+        $text = '';
+        if( isset( $this->tags[ $name ] ) ) 
+        {
+            $text = $this->tags[ $name ];
+            if( func_num_args() > 1 ) {
+                $args = func_get_args();
+                $args[0] = $text;
+                $text = call_user_func_array( 'sprintf', $args );
+            }
+        }
+        return $text;
+    }
+
+    /**
+     * @param array $menu
+     * @param string $ulType
+     * @return string
+     */
+    private function ul( $menu, $ulType='topUl' ) 
+    {
+        $html = $this->text( $ulType );
+        $html .= $this->li( $menu );
+        $html .= $this->text( 'endUl' );
+        return $html;
+    }
+    /**
+     * @param array $menu
+     * @return string
+     */
+    private function li( $menu ) 
+    {
+        $html = '';
+        foreach( $menu as $item ) 
+        {
+            $url   = ( isset( $item[ 'url' ] ) ) ? $item[ 'url' ] : '';
+            $title = ( isset( $item[ 'title' ] ) ) ? $item[ 'title' ] : '';
+            $score = ( isset( $item[ 'score' ] ) ) ? $item[ 'score' ] : 0;
+            $active = ( $score >= $this->max_score ) ? ' active' : '';
+            if( isset( $item[ 'icon' ] ) ) $title = $this->text( 'icon', $item['icon'] ) . $title;
+            if( isset( $item[ 'pages' ] ) ) {
+                $sub = $this->ul( $item['pages'], 'subUl' );
+                $html .= $this->text( 'liSub', $title, $sub, $active );
+            }
+            elseif( isset( $item[ 'divider' ] ) ) {
+                $html .= $this->text( 'divider' );
+            }
+            elseif( isset( $item[ 'target' ] ) && $item[ 'target' ] == 'blank' ) {
+                $html .= $this->text( 'liBlank', $title, $url, $active );
+            }
+            else {
+                $html .= $this->text( 'liItem', $title, $url, $active );
+            }
+        }
+        return $html;
+    }
+}
