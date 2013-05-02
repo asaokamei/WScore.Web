751f1c0d2df407f2641d702a0fc39ea70ec1d880
Asao <kamei@workspot.jp>
2013-05-03 07:14:50 +0900

    added phpdocs comments.

--- src/WScore/Web/Http/Response.php
+++ src/WScore/Web/Http/Response.php
@@ -3,13 +3,31 @@ namespace WScore\Web\Http;
 
 class Response
 {
+    /**
+     * @var string
+     */
     public $content;
+    /**
+     * @var string
+     */
     protected $content_type = 'text/html';
+    /**
+     * @var int
+     */
     protected $status_code = 200;
+    /**
+     * @var array
+     */
     protected $http_headers = array();
-    /** @var string ResponseHelper */
+    
+    /** 
+     * @var string ResponseHelper 
+     */
     protected $helper = '\WScore\Web\Http\ResponseHelper';
 
+    /**
+     * sends out response header and content. 
+     */
     public function send()
     {
         /** @var $helper ResponseHelper */
@@ -25,18 +43,31 @@ class Response
         echo $this->content;
     }
 
+    /**
+     * @param string $content
+     */
     public function setContent( $content ) {
         $this->content = $content;
     }
 
+    /**
+     * @param string $type
+     */
     public function setContentType( $type ) {
         $this->content_type = $type;
     }
 
+    /**
+     * @param int $status_code
+     */
     public function setStatusCode( $status_code ) {
         $this->status_code = $status_code;
     }
 
+    /**
+     * @param string $name
+     * @param string $value
+     */
     public function setHttpHeader( $name, $value ) {
         $this->http_headers[ $name ] = $value;
     }
