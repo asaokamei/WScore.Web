eb060d250d5451347bf75c9e4b4e4376602c3463
Asao <kamei@workspot.jp>
2013-08-12 15:26:51 +0900

    test DispatchAbstract. add DispatchMe as controller and ViewOnly file for testing.

---
+++ tests/WScore/tests/Response/DispatchTest.php
@@ -0,0 +1,41 @@
+<?php
+namespace WScore\tests\Response;
+
+require_once( __DIR__ . '/../../../autoload.php' );
+
+class DispatchTest extends \PHPUnit_Framework_TestCase
+{
+    /** @var  \WScore\Response\Request */
+    public $request;
+
+    /** @var \WScore\tests\Response\Mocks\DispatchMe */
+    public $dispatch;
+
+    public function setUp()
+    {
+        /** @var $container \WScore\DiContainer\Container */
+        $container = include( VENDOR_DIRECTORY . 'wscore/dicontainer/scripts/instance.php' );
+        $container->set( 'ContainerInterface', $container );
+
+        $this->request = $container->get( '\WScore\Response\Request' );
+        $this->dispatch    = $container->get( '\WScore\tests\Response\Mocks\DispatchMe' );
+    }
+    // +----------------------------------------------------------------------+
+
+    function test0()
+    {
+        $this->assertEquals( 'WScore\Response\Request', get_class( $this->request ) );
+        $this->assertEquals( 'WScore\tests\Response\Mocks\DispatchMe', get_class( $this->dispatch ) );
+    }
+
+    function test_dispatch_view_only_file()
+    {
+        $this->dispatch->setRequest( $this->request->uri( 'ViewOnly' ) );
+        /** @var $response \WScore\Response\Response */
+        $response = $this->dispatch->respond();
+
+        $this->assertEquals( 'WScore\Response\Response', get_class( $response ) );
+        $this->assertEquals( __DIR__ . '/Mocks/View/ViewOnly.php', $response->template );
+    }
+
+}
---
+++ tests/WScore/tests/Response/Mocks/DispatchMe.php
@@ -0,0 +1,15 @@
+<?php
+namespace WScore\tests\Response\Mocks;
+
+use WScore\Response\DispatchAbstract;
+
+class DispatchMe extends DispatchAbstract
+{
+    public function __construct( $viewDir=null )
+    {
+        parent::__construct( __DIR__ );
+        $this->setRoute( array(
+            '/ViewOnly' => [],
+        ));
+    }
+}
